fc617b4ad1e07d76eff8641698663b1c460f1cab
youwenhei <youwenhei1110@gmail.com>
2026-07-08 21:43:34 +0800

    Refactor Product constructor and display method
    Refactor Product constructor for improved initialization and update display method for better formatting.

--- src/product.cpp
+++ src/product.cpp
@@ -1,47 +1,38 @@
 #include "Product.h"
 #include <iostream>
 
-using namespace std;
+Product::Product(int product_ID, int product_Barcode, std::string product_Name, std::string product_Description, std::string product_Category,
+    int product_Quantity, double product_Price,
+    std::string product_Supplier, std::string product_ExpiryDate, std::string product_ManufactureDate)
 
-Product::Product(
-	int product_ID, 
-	int product_Barcode, 
-	string product_Name, 
-	string product_Description, 
-	string product_Category,
-	int product_Quantity, 
-	double product_Price,
-	string product_Supplier, 
-	string product_ExpiryDate, 
-	string product_ManufactureDate
-)
-
+    : product_ID(product_ID),
+    product_Barcode(product_Barcode),
+    product_Name(std::move(product_Name)),
+    product_Description(std::move(product_Description)),
+    product_Category(std::move(product_Category)),
+    product_Quantity(product_Quantity),
+    product_Price(product_Price),
+    product_Supplier(std::move(product_Supplier)),
+    product_ExpiryDate(std::move(product_ExpiryDate)),
+    product_ManufactureDate(std::move(product_ManufactureDate))
 {
-	this->product_ID = product_ID;
-	this->product_Barcode = product_Barcode;
-	this->product_Name = product_Name;
-	this->product_Description = product_Description;
-	this->product_Category = product_Category;
-	this->product_Quantity = product_Quantity;
-	this->product_Price = product_Price;
-	this->product_Supplier = product_Supplier;
-	this->product_ExpiryDate = product_ExpiryDate;
-	this->product_ManufactureDate = product_ManufactureDate;
 }
 
-void Product::display()
+void Product::display() const
 {
-	cout << "Plese Key In the Following Product Details:" << endl;
-	cout << "-----------------------------------------" << endl;
-	cout << "Product ID: " << product_ID << endl;
-	cout << "Product Barcode: " << product_Barcode << endl;
-	cout << "Product Name: " << product_Name << endl;
-	cout << "Product Description: " << product_Description << endl;
-	cout << "Product Category: " << product_Category << endl;
-	cout << "Product Quantity: " << product_Quantity << endl;
-	cout << "Product Price: RM" << product_Price << endl;
-	cout << "Product Supplier: " << product_Supplier << endl;
-	cout << "Product Expiry Date: " << product_ExpiryDate << endl;
-	cout << "Product Manufacture Date: " << product_ManufactureDate << endl;
-	cout << "-----------------------------------------" << endl;
+	std::cout << "-------------------\n";
+	std::cout << "Product Details:\n";
+	std::cout << "-------------------\n";
+    std::cout << "Product ID: " << product_ID << "\n"
+        << "Product Barcode: " << product_Barcode << "\n"
+        << "Product Name: " << product_Name << "\n"
+        << "Product Description: " << product_Description << "\n"
+        << "Product Category: " << product_Category << "\n"
+        << "Product Quantity: " << product_Quantity << "\n"
+        << "Product Price: " << product_Price << "\n"
+        << "Product Supplier: " << product_Supplier << "\n"
+        << "Product Expiry Date: " << product_ExpiryDate << "\n"
+        << "Product Manufacture Date: " << product_ManufactureDate << "\n";
+	std::cout << "-------------------\n";
+
 }
