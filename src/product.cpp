df7825a7e282405c99d9a48026eebecc3800816c
youwenhei <youwenhei1110@gmail.com>
2026-07-08 22:55:30 +0800

    Refactor Product constructor and display format

--- src/product.cpp
+++ src/product.cpp
@@ -1,7 +1,7 @@
 #include "Product.h"
 #include <iostream>
 
-Product::Product(int product_ID, int product_Barcode, std::string product_Name, std::string product_Description, std::string product_Category,
+Product::Product(int product_ID, std::string product_Barcode, std::string product_Name, std::string product_Description, std::string product_Category,
     int product_Quantity, double product_Price,
     std::string product_Supplier, std::string product_ExpiryDate, std::string product_ManufactureDate)
 
@@ -15,24 +15,24 @@ Product::Product(int product_ID, int product_Barcode, std::string product_Name, 
     product_Supplier(std::move(product_Supplier)),
     product_ExpiryDate(std::move(product_ExpiryDate)),
     product_ManufactureDate(std::move(product_ManufactureDate))
+
 {
 }
 
 void Product::display() const
 {
-	std::cout << "-------------------\n";
+	std::cout << "====================================================\n";
 	std::cout << "Product Details:\n";
-	std::cout << "-------------------\n";
+	std::cout << "====================================================\n";
     std::cout << "Product ID: " << product_ID << "\n"
         << "Product Barcode: " << product_Barcode << "\n"
         << "Product Name: " << product_Name << "\n"
         << "Product Description: " << product_Description << "\n"
         << "Product Category: " << product_Category << "\n"
         << "Product Quantity: " << product_Quantity << "\n"
-        << "Product Price: " << product_Price << "\n"
+        << "Product Price: RM " << product_Price << "\n"
         << "Product Supplier: " << product_Supplier << "\n"
         << "Product Expiry Date: " << product_ExpiryDate << "\n"
         << "Product Manufacture Date: " << product_ManufactureDate << "\n";
-	std::cout << "-------------------\n";
-
+	std::cout << "====================================================\n";
 }
