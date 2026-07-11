520279ba1332edd1ce34a333eaa7ba3a30a064ed
YOU WEN HEI <youwenhei1110@gmail.com>
2026-07-11 17:40:14 +0800

    Add getMenuOption function for input validation

--- src/utility.cpp
+++ src/utility.cpp
@@ -12,3 +12,24 @@ void pauseScreen(const std::string& message)
 	std::cout << '\n' << message;
 	std::cin.get();
 }
+
+int getMenuOption(int min, int max)
+{
+	int option;
+
+	while (true)
+	{
+		if (std::cin >> option)
+		{
+			if (option >= min && option <= max)
+			{
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				return option;
+			}
+		}
+
+		std::cout << "The input is invalid. Please try again." << std::endl;
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+}
