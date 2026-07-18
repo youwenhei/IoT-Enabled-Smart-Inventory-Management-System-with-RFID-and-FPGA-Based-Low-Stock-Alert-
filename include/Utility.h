aae9197157cc9d5264249bbdf1ad3b48a9ccf855
YOU WEN HEI <youwenhei1110@gmail.com>
2026-07-18 15:40:04 +0800

    Add getCurrentDate function declaration

--- include/Utility.h
+++ include/Utility.h
@@ -7,3 +7,5 @@ void pauseScreen(const std::string& message = "Press Enter to continue...");
 int getMenuOption(int min, int max);
 
 void displayTitle(const std::string& title);
+
+std::string getCurrentDate();
